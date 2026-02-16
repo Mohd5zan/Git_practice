a5e4eaa1f7ba476a0917598cdd2473254d3f334a
Mohd5zan <mohd5zan77@gmail.com>
2026-02-16 12:50:32 +0530

    no smoother animation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+const btn = document.querySelector("button");
+const content = document.querySelector("#slider");
+btn.addEventListener("click", () => {
+  if (content.style.animationPlayState === "paused") {
+    content.style.animationPlayState = "running";
+    btn.innerText = "Stop animation";
+  } else {
+    content.style.animationPlayState = "paused";
+    btn.innerText = "Start animation";
+  }
+});
